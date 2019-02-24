4cfaa1af31de148e8cb1686d0072b877e6f81b78
Thomas Bower <me@thomasbower.net>
2019-02-24 20:22:22 +0000

    Start checking outgoing requests for URL parameters containing form entries

--- extension/background.js
+++ extension/background.js
@@ -1,13 +1,36 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
+chrome.webRequest.onBeforeRequest.addListener(
+    function(info) {
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {type: "getInputValues"},
+                    inputs => findInputsInURL(inputs, new URL(info.url)));
+        });
+
+        return {}
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking", "requestBody"]
+);
+
+function findInputsInURL(inputs, url) {
+    url.searchParams.forEach(param => {
+        inputs.map(input => input.value).forEach(input => {
+            if (isBase64Encoded(param)) {
+                const decoded = atob(param);
+                if (decoded.indexOf(input) !== -1) {
+                    console.log(`Potentially found a skimmer with input value ${input} and URL parameter ${param} (decoded to ${decoded})`);
+                    return true;
+                }
+            }
+            if (param.indexOf(input) !== -1) {
+                console.log(`Potentially found a skimmer with input value ${input} and URL parameter ${param}`);
+                return true;
+            }
+        })
     });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
+    return false;
+}
+
+function isBase64Encoded(string) {
+    return /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/.test(string);
+}
---
+++ extension/content.js
@@ -0,0 +1,24 @@
+const cardHolderName = /card.?holder|name.*\bon\b.*card|cc.?name|cc.?full.?name|owner|karteninhaber|nombre.*tarjeta|nom.*carte|nome.*cart|名前|Имя.*карты|信用卡开户名|开户名|持卡人姓名|持卡人姓名/i;
+
+function log(message, logType = 'log') {
+    if (logType !== 'log' && logType !== 'error' && logType !== 'info' && logType !== 'warn') return;
+    console[logType]('[SkimmerDetector] ', message);
+}
+
+function getPopulatedInputValues() {
+    return [...document.querySelectorAll("input, textarea")].map(input => ({
+        id: input.id || "",
+        name: input.name || "",
+        value: input.value || ""
+    })).filter(input => input.value);
+}
+
+log(getPopulatedInputValues());
+
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.type === "getInputValues") {
+            sendResponse(getPopulatedInputValues());
+        }
+    }
+);
