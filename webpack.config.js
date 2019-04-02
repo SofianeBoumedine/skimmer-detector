f81f6e9e14bf14b9adb139628791b469a23ab06f
Thomas Bower <me@thomasbower.net>
2019-04-02 16:38:13 +0100

    Refactor to use webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// Dependencies
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// constants
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
+const CONTENT_SCRIPTS_DIR = path.resolve(__dirname, 'app/content-scripts');
+
+// webpack plugins
+
+const buildPlugins = [
+  new CleanWebpackPlugin(),
+];
+
+// configs
+module.exports = {
+  entry: {
+    background: [`${SRC_DIR}/background.js`],
+    content: [`${CONTENT_SCRIPTS_DIR}/content.js`],
+    content_before: [`${CONTENT_SCRIPTS_DIR}/content_before.js`],
+  },
+  devtool: 'none',
+  performance: { hints: false },
+  output: {
+    filename: '[name].js',
+    path: BUILD_DIR,
+  },
+  resolve: {
+    symlinks: false,
+    extensions: ['.js'],
+  },
+  plugins: buildPlugins,
+};
